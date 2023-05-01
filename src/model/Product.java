f22c008cc5ef01c89e6d6d8828daedfcd31dcc14
MGMaikeru <13miguegonza2005@gmail.com>
2023-05-01 12:46:50 -0500

    Update parcial order register

--- src/model/Product.java
+++ src/model/Product.java
@@ -79,4 +79,23 @@ public class Product {
 				"}";
 	}
 
+	public Product(String productName, String description, int quanAvailable, int price) {
+		this.productName = productName;
+		this.description = description;
+		this.quanAvailable = quanAvailable;
+		this.price = price;
+		this.purchases = 0;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public int getQuanAvailable() {
+		return quanAvailable;
+	}
+
+	public void setQuanAvailable(int quanAvailable) {
+		this.quanAvailable = quanAvailable;
+	}
 }
